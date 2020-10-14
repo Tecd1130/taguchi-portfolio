65d9091f75a668b4e5d4b2349ab2ce89c1466f52
Endo Takaki <epk1510@gmail.com>
2020-10-14 18:16:05 +0900

    不要ファイル削除＆ヘッダーコンポーネント作成

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./components/index";
+import "./assets/css/reset.css";
+import "./assets/css/style.scss";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import HeaderLogo from "../assets/img/logo_header.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <h1 className="header-logo">
+          <img src={HeaderLogo} alt="i am" />
+        </h1>
+
+        <input id="headerMenu" type="checkbox" className="header-nav-check" />
+        <label htmlFor="headerMenu" className="header-nav-toggle">
+          <span></span>
+        </label>
+
+        <nav className="header-nav">
+          <ul className="header-nav-list">
+            <li className="header-nav-item">
+              <a href="/works/">実績</a>
+            </li>
+            <li className="header-nav-item">
+              <a href="/profile/">プロフィール</a>
+            </li>
+            <li className="header-nav-item">
+              <a href="/contact/">お問い合せ</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
