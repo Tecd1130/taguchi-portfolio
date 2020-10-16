e3059d6e6008174af0971523a0a989c1f4ac3525
Endo Takaki <epk1510@gmail.com>
2020-10-16 19:54:03 +0900

    firebase初期設定

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDR9hxC7bKqmXvAuxm1mv4u1csSYtCY8U",
+  authDomain: "taguchi-portfolio.firebaseapp.com",
+  databaseURL: "https://taguchi-portfolio.firebaseio.com",
+  projectId: "taguchi-portfolio",
+  storageBucket: "taguchi-portfolio.appspot.com",
+  messagingSenderId: "778482359589",
+  appId: "1:778482359589:web:d6717dbcc74490c6638ecc",
+};
