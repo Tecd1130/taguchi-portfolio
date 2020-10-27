1493184339015f8609d3f5e63c1bb951b9dd5589
Endo Takaki <epk1510@gmail.com>
2020-10-28 08:45:44 +0900

    タブ表示機能実装

--- src/components/WorksCard.jsx
+++ src/components/WorksCard.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const WorksCard = (props) => {
   return (
-    <a className="works-card" href={"/works/detail/" + props.id}>
+    <a
+      className={"works-card" + " " + props.category}
+      href={"/works/detail/" + props.id}
+    >
       <img className="works-card-img" src={props.thumb} alt="" />
       <div className="works-card-info">
         <p className="works-card-client">{props.client}</p>
--- src/templates/Works.jsx
+++ src/templates/Works.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Header, Footer, WorksCard } from "../components/index";
-// import IconAll from "../assets/img/icon_all.png";
-// import IconWeb from "../assets/img/icon_web.png";
-// import IconBanner from "../assets/img/icon_banner.png";
-// import IconIllustWorks from "../assets/img/icon_illust_works.png";
-// import IconLogoWorks from "../assets/img/icon_logo_works.png";
-// import IconPlan from "../assets/img/icon_plan.png";
+import IconAll from "../assets/img/icon_all.png";
+import IconWeb from "../assets/img/icon_web.png";
+import IconBanner from "../assets/img/icon_banner.png";
+import IconIllustWorks from "../assets/img/icon_illust_works.png";
+import IconLogoWorks from "../assets/img/icon_logo_works.png";
+import IconPlan from "../assets/img/icon_plan.png";
 import { db } from "../firebase";
 
 const Works = () => {
@@ -25,6 +25,74 @@ const Works = () => {
     fetchWorks();
   }, []);
 
+  const tabAll = () => {
+    const worksCard = document.getElementsByClassName("works-card");
+
+    for (let i = 0; i < worksCard.length; i++) {
+      worksCard[i].style.display = "block";
+    }
+  };
+
+  const tabWeb = () => {
+    const worksCard = document.getElementsByClassName("works-card");
+    const web = document.getElementsByClassName("web");
+
+    for (let i = 0; i < worksCard.length; i++) {
+      worksCard[i].style.display = "none";
+    }
+    for (let j = 0; j < web.length; j++) {
+      web[j].style.display = "block";
+    }
+  };
+
+  const tabBanner = () => {
+    const worksCard = document.getElementsByClassName("works-card");
+    const banner = document.getElementsByClassName("banner");
+
+    for (let i = 0; i < worksCard.length; i++) {
+      worksCard[i].style.display = "none";
+    }
+    for (let j = 0; j < banner.length; j++) {
+      banner[j].style.display = "block";
+    }
+  };
+
+  const tabLogo = () => {
+    const worksCard = document.getElementsByClassName("works-card");
+    const logo = document.getElementsByClassName("logo");
+
+    for (let i = 0; i < worksCard.length; i++) {
+      worksCard[i].style.display = "none";
+    }
+    for (let j = 0; j < logo.length; j++) {
+      logo[j].style.display = "block";
+    }
+  };
+
+  const tabIllust = () => {
+    const worksCard = document.getElementsByClassName("works-card");
+    const illust = document.getElementsByClassName("illust");
+
+    for (let i = 0; i < worksCard.length; i++) {
+      worksCard[i].style.display = "none";
+    }
+    for (let j = 0; j < illust.length; j++) {
+      illust[j].style.display = "block";
+    }
+  };
+
+  const tabPlan = () => {
+    const worksCard = document.getElementsByClassName("works-card");
+    const plan = document.getElementsByClassName("plan");
+
+    for (let i = 0; i < worksCard.length; i++) {
+      worksCard[i].style.display = "none";
+    }
+    for (let j = 0; j < plan.length; j++) {
+      plan[j].style.display = "block";
+    }
+  };
+
   return (
     <>
       <Header></Header>
@@ -33,8 +101,8 @@ const Works = () => {
           <h2 className="section-title">
             実績<span>Works</span>
           </h2>
-          {/* <ul className="works-tab-list">
-            <li className="works-tab-item">
+          <ul className="works-tab-list">
+            <li className="works-tab-item" onClick={tabAll}>
               <img
                 className="works-tab-img works-tab-img--all"
                 src={IconAll}
@@ -42,7 +110,7 @@ const Works = () => {
               />
               <p className="works-tab-text">すべて</p>
             </li>
-            <li className="works-tab-item">
+            <li className="works-tab-item" onClick={tabWeb}>
               <img
                 className="works-tab-img works-tab-img--web"
                 src={IconWeb}
@@ -50,7 +118,7 @@ const Works = () => {
               />
               <p className="works-tab-text">Web</p>
             </li>
-            <li className="works-tab-item">
+            <li className="works-tab-item" onClick={tabBanner}>
               <img
                 className="works-tab-img works-tab-img--banner"
                 src={IconBanner}
@@ -58,7 +126,7 @@ const Works = () => {
               />
               <p className="works-tab-text">バナー</p>
             </li>
-            <li className="works-tab-item">
+            <li className="works-tab-item" onClick={tabLogo}>
               <img
                 className="works-tab-img works-tab-img--logo"
                 src={IconLogoWorks}
@@ -66,7 +134,7 @@ const Works = () => {
               />
               <p className="works-tab-text">ロゴデザイン</p>
             </li>
-            <li className="works-tab-item">
+            <li className="works-tab-item" onClick={tabIllust}>
               <img
                 className="works-tab-img works-tab-img--illust"
                 src={IconIllustWorks}
@@ -74,7 +142,7 @@ const Works = () => {
               />
               <p className="works-tab-text">イラスト</p>
             </li>
-            <li className="works-tab-item">
+            <li className="works-tab-item" onClick={tabPlan}>
               <img
                 className="works-tab-img works-tab-img--plan"
                 src={IconPlan}
@@ -82,7 +150,7 @@ const Works = () => {
               />
               <p className="works-tab-text">写真商品企画</p>
             </li>
-          </ul> */}
+          </ul>
 
           <div className="works-card-list">
             {works.length > 0 &&
@@ -93,6 +161,7 @@ const Works = () => {
                   client={work.client}
                   title={work.title}
                   thumb={work.thumbSP}
+                  category={work.category}
                 />
               ))}
           </div>
